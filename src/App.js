9307a0a4be488af5ad1d65e725514e4e7bccc994
amayorgad <almade87@gmail.com>
2021-10-27 20:10:22 -0500

    Clase 1 de React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import MiPrimerComponente from './components/MiPrimerComponente';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MiPrimerComponente/>
         <img src={logo} className="App-logo" alt="logo" />
+        <h4>Hola Mundo</h4>
+        <h5>Hola Mundo</h5>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/MiPrimerComponente.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class MiPrimerComponente extends Component{
+
+     render(){
+        return(
+            <div>
+                <h1>Mi Primer Componente</h1>
+                <h2>Subtitulo del Componente</h2>
+            </div>
+        );
+    }
+
+}
+
+export default MiPrimerComponente;
